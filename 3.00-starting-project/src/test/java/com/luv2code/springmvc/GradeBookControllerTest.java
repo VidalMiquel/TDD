ffbe0060bebfc3c38e43fc7291761e4f3cbf4fcd
Miquel Vidal <mvidalc@riu.com>
2024-10-11 13:44:51 +0200

    DDBB tests done

---
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/GradeBookControllerTest.java
@@ -0,0 +1,72 @@
+package com.luv2code.springmvc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.ModelAndViewAssert;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.luv2code.springmvc.models.CollegeStudent;
+import com.luv2code.springmvc.models.GradebookCollegeStudent;
+import com.luv2code.springmvc.service.StudentAndGradeService;
+
+@TestPropertySource("/application.properties")
+@AutoConfigureMockMvc
+@SpringBootTest
+public class GradeBookControllerTest {
+
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @Mock
+    private StudentAndGradeService studentAndGradeServiceMock;
+
+    @BeforeEach
+    public void beforeEach() {
+        jdbc.execute("insert into student(id, firstname, lastname, email_address) " +
+                "values (1, 'Eric', 'Roby', 'eric.roby@luv2code_school.com')");
+    }
+
+    @Test
+    public void getStudentHttpRequest() throws Exception{
+        CollegeStudent studentOne = new GradebookCollegeStudent("Eric", "Roby", "eric_roby@riu.com");
+        CollegeStudent studentTwo = new GradebookCollegeStudent("Chad", "Darby", "chad_darby@riu.com");
+        
+        List<CollegeStudent> collegeStudentList = new ArrayList<>(Arrays.asList(studentOne,studentTwo));
+
+        when(studentAndGradeServiceMock.getGradebook()).thenReturn(collegeStudentList);
+
+        assertIterableEquals(collegeStudentList, studentAndGradeServiceMock.getGradebook());
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(status().isOk()).andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView();
+
+        ModelAndViewAssert.assertViewName(mav, "index");
+    }
+
+    @AfterEach
+    public void setupAfterTransaction() {
+        jdbc.execute("DELETE FROM student");
+    }
+
+}
